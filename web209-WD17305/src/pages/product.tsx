e4b8ea1c824c8bd1b2739ac7daa2d5c96cf957a1
cuongpqph24058 <114381515+cuongpqph24058@users.noreply.github.com>
2023-08-08 12:54:52 +0700

    up trang chủ

--- web209-WD17305/src/pages/product.tsx
+++ web209-WD17305/src/pages/product.tsx
@@ -75,3 +75,82 @@ const ProductDetailPage = () => {
                 Mô tả ngắn: Trước khi mua bất kỳ chiếc điện thoại nào, người
                 dùng cũng sẽ quan tâm đến thiết kế sản phẩm trước. Với phiên bản
                 A73, Samsung đã tạo nên một chiếc smartphone với vẻ ngoài mang
+                đến cảm giác sang trọng và tinh tế.
+              </span>
+            </div>
+          </div>
+        </div>
+
+        {/* đặc điểm SẢN PHẨM */}
+        <div className="w-[1100px] mx-[160px]">
+          <div className="bg-[#F2F2F2] mb-[13px] py-[20px]">
+            <h1 className="text-center text-red-500 font-bold">
+              ĐẶC ĐIỂM NỔI BẬT
+            </h1>
+            <p className="ml-[20px] pt-[5px]">
+              Camera chất lượng, bắt trọn từng khoảng khắc - Cụm 4 camera với
+              cảm biến chính lên đến 108 MP
+            </p>
+            <p className="ml-[20px] pt-[5px]">
+              Thưởng thức không gian giải trí cực đỉnh - Màn hình lớn 6.7 inch,
+              độ phân giải Full HD+, 120Hz mượt mà
+            </p>
+            <p className="ml-[20px] pt-[5px]">
+              Cấu hình Galaxy A73 5G được nâng cấp mạnh với chip Snapdragon
+              778G, RAM lên đến 8 GB
+            </p>
+            <p className="ml-[20px] pt-[5px]">
+              Chiến game thoải mái không lo gián đoạn - Viên pin lớn 5000 mAh,
+              hỗ trợ sạc nhanh 25 W
+            </p>
+          </div>
+
+          <div className="mt-[15px] mb-[29px]">
+            <span>
+              Năm 2022 hứa hẹn sẽ là một năm rất đáng trông đợi đối với những ai
+              là fan của thương hiệu điện thoại Samsung. Mới đây, hãng sẽ tiếp
+              tục cho ra mắt nhiều smartphone với sự cải tiến trong thiết kế và
+              cấu hình, trong đó phải kể đến chiếc Samsung Galaxy A73 với nhiều
+              cải tiến so với thế hệ trước. Vậy sản phẩm có gì nổi bật, giá bao
+              nhiêu và liệu có nên mua không? Tìm hiểu ngay nhé!
+            </span>
+          </div>
+
+          <div className="mb-[30px]">
+            <h2 className="font-bold">
+              Đánh giá Samsung A73 - Hiệu năng mượt mà, chụp ảnh chuyên nghiệp
+            </h2>
+            <span>
+              Điện thoại cao cấp nhất dòng Galaxy A series sở hữu nhiều nâng cấp
+              đáng giá so với thế hệ trước, từ ngoại hình cho đến hiệu năng, đặc
+              biệt là hệ thống camera. Sau đây là những đánh giá chi tiết về
+              chiếc
+            </span>
+          </div>
+
+          <div>
+            <h2 className="font-bold">
+              Thiết kế sang trọng, màn hình Super AMOLED
+            </h2>
+            <span>
+              Trước khi mua bất kỳ chiếc điện thoại nào, người dùng cũng sẽ quan
+              tâm đến thiết kế sản phẩm trước. Với phiên bản A73, Samsung đã tạo
+              nên một chiếc smartphone với vẻ ngoài mang đến cảm giác sang
+              trọngvà tinh tế. Samsung Galaxy A73 được thiết kế gọn nhẹ với tiêu
+              chí đáp ứng khả năng mang theo để tiện đi lại cho người dùng. Giờ
+              đây, bạn có thể mang theo chiếc smartphone bên cạnh đến bất cứ
+              đâu, bất cứ lúc nào. Kích thước và trọng lượng của chiếc điện
+              thoại rất vừa phải và dĩ nhiên sẽ không chiếm quá nhiều diện tích
+              trong túi xách và có thể di chuyển dễ dàng.
+            </span>
+          </div>
+          <button className="w-[350px] h-[40px] text-center border-2 rounded-xl translate-x-80 mt-[20px]">
+            Xem thêm
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductDetailPage;
